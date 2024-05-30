bbae8721aa01b6b93fe6def1c44d2f7a8044338c
jqkk <kkj2551@naver.com>
2024-05-30 11:26:43 +0900

    Update

---
+++ src/main/kotlin/code/diagonal-difference.kt
@@ -0,0 +1,20 @@
+package code
+
+import kotlin.math.abs
+
+val diagonalDifference = { arr: Array<Array<Int>> ->
+    val leftDiagonalSum = arr.indices.sumOf { arr[it][it] }
+    val rightDiagonalSum = arr.indices.sumOf { arr[it][arr.lastIndex - it] }
+    abs(leftDiagonalSum - rightDiagonalSum)
+}
+
+fun main() {
+    val n = readln().trim().toInt()
+
+    val arr = Array(n) { Array<Int>(n) { 0 } }.map { readln().trim().split(" ").map { it.toInt() }.toTypedArray() }
+        .toTypedArray()
+
+    val result = diagonalDifference(arr)
+
+    println(result)
+}
