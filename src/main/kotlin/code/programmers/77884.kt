bdb1a95d6df2d9c171b47244378e514378154568
jqkk <kkj2551@naver.com>
2024-06-04 15:25:32 +0900

    Update

---
+++ src/main/kotlin/code/programmers/77884.kt
@@ -0,0 +1,15 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/77884
+package code.programmers
+
+import kotlin.math.sqrt
+
+fun solution(left: Int, right: Int): Int {
+    return (left..right).toList().toIntArray()
+        .reduce { acc, i -> if (sqrt(i.toDouble()).let { it == it.toInt().toDouble() }) acc - i else acc + i }
+}
+
+
+fun main() {
+    val result = solution(1, 1000)
+    println(result)
+}
--- src/main/kotlin/code/programmers/92334.kt
+++ src/main/kotlin/code/programmers/92334.kt
@@ -1,29 +1,25 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=kotlin
 package code.programmers
 
-
-class Solution {
-    fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
-        val logs = report.toSet().map {
-            val log = it.split(" ")
-            log[0] to log[1]
-        }
-
-        val targetUserCount = logs.groupBy { it.second }.filter {
-            it.value.count() >= k
-        }.map {
-            it.value
-        }.flatten().map { it.first }.groupingBy { it }.eachCount()
-
-        return id_list.map {
-            targetUserCount[it] ?: 0
-        }.toIntArray()
+fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
+    val logs = report.toSet().map {
+        val log = it.split(" ")
+        log[0] to log[1]
     }
+
+    val targetUserCount = logs.groupBy { it.second }.filter {
+        it.value.count() >= k
+    }.map {
+        it.value
+    }.flatten().map { it.first }.groupingBy { it }.eachCount()
+
+    return id_list.map {
+        targetUserCount[it] ?: 0
+    }.toIntArray()
 }
 
 fun main() {
-    val solution = Solution()
-    val result = solution.solution(
+    val result = solution(
         arrayOf("muzi", "frodo", "apeach", "neo"),
         arrayOf("muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"), 2
     )
