ae821f75e4d14e6546361c0aefba03b27b1173e9
jqkk <kkj2551@naver.com>
2024-06-04 15:36:36 +0900

    Update

--- src/main/kotlin/code/programmers/77884.kt
+++ src/main/kotlin/code/programmers/77884.kt
@@ -5,11 +5,11 @@ import kotlin.math.sqrt
 
 fun solution(left: Int, right: Int): Int {
     return (left..right)
-        .reduce { acc, i -> if (sqrt(i.toDouble()).let { it == it.toInt().toDouble() }) acc - i else acc + i }
+        .fold(0) { acc, i -> if (sqrt(i.toDouble()).let { it == it.toInt().toDouble() }) acc - i else acc + i }
 }
 
 
 fun main() {
-    val result = solution(1, 1000)
+    val result = solution(4, 5)
     println(result)
 }
