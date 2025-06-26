4a7cc9ffd7008aba76d51e2642f23e496d926888
jqkk <kkj2551@naver.com>
2025-06-27 00:55:57 +0900

    Update

---
+++ src/main/kotlin/code/programmers/142086.kt
@@ -0,0 +1,23 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/142086
+package code.programmers
+
+fun solution(s: String): IntArray {
+    val tempList = mutableListOf<Char>()
+    val result = IntArray(s.length)
+
+    for (i in s.indices) {
+        val currentValue = s[i]
+        val absoluteIndex = tempList.lastIndexOf(currentValue)
+
+        result[i] = if (absoluteIndex == -1) -1 else i - absoluteIndex
+
+        tempList.add(currentValue)
+    }
+
+    return result
+}
+
+fun main() {
+    println(solution("banana").contentToString())
+    println(solution("foobar").contentToString())
+}
